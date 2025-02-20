83cebdb7f314e2836cc17e44a3d0ecc82888b0cf
Dean Cullen <Dean_cullen@next.co.uk>
2025-02-20 16:09:00 +0000

    More formatting

--- src/gatling/simulations/scenarios/ET_CaseDocUpload2.scala
+++ src/gatling/simulations/scenarios/ET_CaseDocUpload2.scala
@@ -10,11 +10,8 @@ object ET_CaseDocUpload2 {
 
   
   val IdamURL = Environment.idamURL
-
-
   val MinThinkTime = Environment.minThinkTime
   val MaxThinkTime = Environment.maxThinkTime
-
   val CommonHeader = Environment.commonHeader
 
   val postcodeFeeder = csv("postcodes.csv").circular
@@ -24,11 +21,10 @@ object ET_CaseDocUpload2 {
   exec(_.setAll(
       "ETCWRandomString" -> (Common.randomString(7))))
   
-  
-    
     /*======================================================================================
-                   * Create ET - Upload around 11 docs to make it 20-25 documents for case file view
-        ==========================================================================================*/
+     Create ET - Upload around 11 docs to make it 20-25 documents for case file view
+     ==========================================================================================*/
+
     .group("ET_CW_040_UploadDocs") {
       exec(http("ET_CW_040_StartUploadDoc")
         .get("/data/internal/cases/#{caseId}/event-triggers/uploadDocument?ignore-warning=false")
@@ -36,15 +32,13 @@ object ET_CaseDocUpload2 {
         .header("accept", "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-start-event-trigger.v2+json;charset=UTF-8")
         .check(substring("Upload Document"))
         .check(status.is(200))
-        .check(jsonPath("$.event_token").optional.saveAs("event_token"))
-      )
-        .pause(MinThinkTime, MaxThinkTime)
+        .check(jsonPath("$.event_token").optional.saveAs("event_token")))
     }
+    .pause(MinThinkTime, MaxThinkTime)
   
   /*======================================================================================
-                   * Create ET - upload document
-        ==========================================================================================*/
-  
+  Create ET - upload document
+  ==========================================================================================*/
     /*======================================================================================
     * Upload ACAS
     ======================================================================================*/
@@ -65,37 +59,9 @@ object ET_CaseDocUpload2 {
         .formParam("caseTypeId", "#{caseId}")
         .formParam("jurisdictionId", "EMPLOYMENT")
         .check(jsonPath("$.documents[0]._links.self.href").saveAs("documentURL_ACAS2"))
-        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_ACAS2"))
-        
-      )
-       // .exec(Common.userDetails)
-       // .exec(Common.userDetails)
+        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_ACAS2")))
     }
-  
     .pause(MinThinkTime, MaxThinkTime)
-  
-  
-    /*  .group("CUI_CreateClaim_155_Upload") {
-           exec(http("CUI_CreateClaim_155_Upload")
-             .post(BaseURL + "/documents")
-             .headers(Headers.commonHeader)
-             .headers(Headers.postHeader)
-             .header("accept", "application/json")
-             .header("content-type", "multipart/form-data; boundary=----WebKitFormBoundaryGPNRb3mRRAhkFBRV")
-             .header("sec-fetch-dest", "empty")
-             .header("sec-fetch-mode", "cors")
-             .bodyPart(RawFileBodyPart("files[]", "2MB.pdf")
-               .fileName("2MB.pdf")
-               .transferEncoding("binary"))
-             .asMultipartForm
-           //  .check(jsonPath("$[0].id").saveAs("Document_ID"))
-             .check(substring("2MB.pdf")))
-         }
-         .pause(MinThinkTime, MaxThinkTime)
-   
-        */
-  
-   
   
     /*======================================================================================
     * Upload Acknowledgement
@@ -116,15 +82,10 @@ object ET_CaseDocUpload2 {
         .formParam("caseTypeId", "#{caseId}")
         .formParam("jurisdictionId", "EMPLOYMENT")
         .check(jsonPath("$.documents[0]._links.self.href").saveAs("documentURL_Ack2"))
-        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_Ack2"))
-      )
-      // .exec(Common.userDetails)
-      // .exec(Common.userDetails)
+        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_Ack2")))
     }
-  
     .pause(MinThinkTime, MaxThinkTime)
-  
-   
+
     /*======================================================================================
     * Upload Notice
     ======================================================================================*/
@@ -144,15 +105,10 @@ object ET_CaseDocUpload2 {
         .formParam("caseTypeId", "#{caseId}")
         .formParam("jurisdictionId", "EMPLOYMENT")
         .check(jsonPath("$.documents[0]._links.self.href").saveAs("documentURL_Notice2"))
-        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_Notice2"))
-      )
-      // .exec(Common.userDetails)
-      // .exec(Common.userDetails)
+        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_Notice2")))
     }
-  
     .pause(MinThinkTime, MaxThinkTime)
   
-   
     /*======================================================================================
     * Upload TribunalCase
     ======================================================================================*/
@@ -172,15 +128,10 @@ object ET_CaseDocUpload2 {
         .formParam("caseTypeId", "#{caseId}")
         .formParam("jurisdictionId", "EMPLOYMENT")
         .check(jsonPath("$.documents[0]._links.self.href").saveAs("documentURL_TribunalCaseFile2"))
-        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_TribunalCaseFile2"))
-      )
-      // .exec(Common.userDetails)
-      // .exec(Common.userDetails)
+        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_TribunalCaseFile2")))
     }
-  
     .pause(MinThinkTime, MaxThinkTime)
   
-   
     /*======================================================================================
     * Upload TribunalOrder
     ======================================================================================*/
@@ -200,15 +151,9 @@ object ET_CaseDocUpload2 {
         .formParam("caseTypeId", "#{caseId}")
         .formParam("jurisdictionId", "EMPLOYMENT")
         .check(jsonPath("$.documents[0]._links.self.href").saveAs("documentURL_TribunalOrder2"))
-        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_TribunalOrder2"))
-      )
-      // .exec(Common.userDetails)
-      // .exec(Common.userDetails)
+        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_TribunalOrder2")))
     }
-  
     .pause(MinThinkTime, MaxThinkTime)
-  
-    
   
     /*======================================================================================
     * Upload Other
@@ -229,12 +174,8 @@ object ET_CaseDocUpload2 {
         .formParam("caseTypeId", "#{caseId}")
         .formParam("jurisdictionId", "EMPLOYMENT")
         .check(jsonPath("$.documents[0]._links.self.href").saveAs("documentURL_Other2"))
-        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_Other2"))
-      )
-      // .exec(Common.userDetails)
-      // .exec(Common.userDetails)
+        .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken_Other2")))
     }
-  
     .pause(MinThinkTime, MaxThinkTime)
   
     /*======================================================================================
@@ -249,16 +190,13 @@ object ET_CaseDocUpload2 {
         .header("x-xsrf-token", "${XSRFToken}")
         .body(ElFileBody("bodies/docupload/docupload2.json"))
         .check(substring("documentCollection")))
-      
-       // .exec(Common.userDetails)
-       // .exec(Common.userDetails)
-    }
-  
+    }  
     .pause(MinThinkTime, MaxThinkTime)
   
   /*======================================================================================
-      * Click Submit
-      ======================================================================================*/
+  * Click Submit
+  ======================================================================================*/
+
     .group("CD_CreateClaim_400_NotifyDetailsEventSubmit") {
       exec(http("CD_CreateClaim_370_EventSubmit")
         .post("/data/cases/#{caseId}/events")
@@ -267,14 +205,13 @@ object ET_CaseDocUpload2 {
         .header("x-xsrf-token", "${XSRFToken}")
         .body(ElFileBody("bodies/docupload/uploadsubmit2.json"))
         .check(substring("#{caseId}"))
-        .check(status.in(200, 201))
-      )
-        .exec(http("CD_CreateClaim_400_010_case")
-          .get("/data/internal/cases/#{caseId}")
-          .headers(CommonHeader)
-          .header("accept", "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-case-view.v2+json")
-          .check(status.in(200, 201, 204, 304))
-        )
+        .check(status.in(200, 201)))
+
+      .exec(http("CD_CreateClaim_400_010_case")
+        .get("/data/internal/cases/#{caseId}")
+        .headers(CommonHeader)
+        .header("accept", "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-case-view.v2+json")
+        .check(status.in(200, 201, 204, 304)))
     }
     .pause(MinThinkTime, MaxThinkTime)
   
