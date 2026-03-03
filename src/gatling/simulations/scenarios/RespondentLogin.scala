79f3b88e4829311b3359d02c0bece4a07677b7c6
pkryonas <paris.kryonas@solirius.com>
2026-03-03 17:03:39 +0000

    Upgrading Gatling and Gradle versions, including all necessary changes.

--- src/gatling/simulations/scenarios/RespondentLogin.scala
+++ src/gatling/simulations/scenarios/RespondentLogin.scala
@@ -54,12 +54,12 @@ object RespondentLogin {
 
     group("XUI_020_Login") {
       exec(http("XUI_020_005_Login")
-        .post(IdamUrl + "/login?client_id=xuiwebapp&redirect_uri=" + XuiUrl + "/oauth2/callback&state=${state}&nonce=${nonce}&response_type=code&scope=profile%20openid%20roles%20manage-user%20create-user%20search-user&prompt=")
-        .formParam("username", "${user}")
-        .formParam("password", "${password}")
+        .post(IdamUrl + "/login?client_id=xuiwebapp&redirect_uri=" + XuiUrl + "/oauth2/callback&state=#{state}&nonce=#{nonce}&response_type=code&scope=profile%20openid%20roles%20manage-user%20create-user%20search-user&prompt=")
+        .formParam("username", "#{user}")
+        .formParam("password", "#{password}")
         .formParam("save", "Sign in")
         .formParam("selfRegistrationEnabled", "false")
-        .formParam("_csrf", "${csrf}")
+        .formParam("_csrf", "#{csrf}")
         //.headers(Headers.commonHeader)
         .headers(Headers.postHeader)
         .check(regex("Manage cases")))
@@ -79,7 +79,7 @@ object RespondentLogin {
         .exec(Common.monitoringTools)
 
           //if there is no in-flight case, set the case to 0 for the activity calls
-      .doIf("${caseId.isUndefined()}") {
+      .doIf("#{caseId.isUndefined()}") {
         exec(_.set("caseId", "0"))
       }
 
@@ -94,17 +94,17 @@ object RespondentLogin {
       .exec(Common.orgDetails)
       
       .exec(http("XUI_020_015_WorkBasketInputs")
-        .get("/data/internal/case-types/${caseType}/work-basket-inputs")
+        .get("/data/internal/case-types/#{caseType}/work-basket-inputs")
         .headers(Headers.commonHeader)
         .header("accept", "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-workbasket-input-details.v2+json;charset=UTF-8")
         .check(regex("workbasketInputs|Not Found"))
         .check(status.in(200, 404)))
 
       .exec(http("XUI_020_020_SearchCases")
-        .post("/data/internal/searchCases?ctid=${caseType}&use_case=WORKBASKET&view=WORKBASKET&page=1")
+        .post("/data/internal/searchCases?ctid=#{caseType}&use_case=WORKBASKET&view=WORKBASKET&page=1")
         .headers(Headers.commonHeader)
         .header("accept", "application/json")
-        .formParam("x-xsrf-token", "${XSRFToken}")
+        .formParam("x-xsrf-token", "#{XSRFToken}")
         .body(StringBody("""{"size":25}"""))
         .check(substring("columns")))
 
